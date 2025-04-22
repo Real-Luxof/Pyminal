3ab98603d42f084268b7f04df0c6c1ed760510ff
mafnankhattak269 <mafnankhattak260@gmail.com>
2025-04-22 15:38:49 +0500

    Oops

---
+++ fonts/font_size_requirements.py
@@ -0,0 +1,11 @@
+font_size_requirements = {
+    "aflfont.ttf": 12,
+    "consolas-mono.ttf": 12,
+    "gabriola.ttf": 16,
+    "garamond.ttf": 16,
+    
+    "aflfont": 12,
+    "consolas-mono": 12,
+    "gabriola": 16,
+    "garamond": 16
+}
